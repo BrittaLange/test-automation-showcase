1bef889024b9f5140de6d3540747b68fe1007d8f
BrittaLange <britta.lange@mailbox.org>
2026-02-18 20:21:28 +0100

    Implemented test case in cypress Refs #18

--- e2e-tests/cypress/cypress/e2e/tc-crud-001.cy.ts
+++ e2e-tests/cypress/cypress/e2e/tc-crud-001.cy.ts
@@ -1,10 +1,35 @@
-describe('template spec', () => {
-  it('passes', () => {
+import { contains } from "../../node_modules/cypress/types/jquery/index"
+
+describe('Create customer with valid data', () => {
+
+  afterEach(() => {
+    // Clean up and delete the created customer.
+    cy.get('table tbody tr')
+      .last()
+      .find('td').contains('Delete').click()
+  })
+
+  it('creates a customer with valid data', () => {
     cy.visit('http://localhost/simple-crud-webapp/app/')
     const name = 'John Wayne'
     const location = 'Denver'
     cy.get('[name="name"]').type(name)
     cy.get('[name="location"]').type(location)
     cy.get('[name="save"]').click()
+
+    // Expect success message
+    cy.get('.alert').should("contain", "New customer has been saved.")
+    // Expect customer to be displayed in table.
+    cy.get('table tbody tr')
+      .last()
+      .find('td')
+      .eq(0) // First column
+      .should('contain', name)
+
+    cy.get('table tbody tr')
+      .last()
+      .find('td')
+      .eq(1) // Second column
+      .should('contain', location)
   })
 })
