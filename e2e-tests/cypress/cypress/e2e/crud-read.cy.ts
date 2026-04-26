16f79bbb8532edd0def07c29bad30970163eaacb
BrittaLange <britta.lange@mailbox.org>
2026-04-26 07:05:46 +0200

    Integrated commands Resolves #37

--- e2e-tests/cypress/cypress/e2e/crud-read.cy.ts
+++ e2e-tests/cypress/cypress/e2e/crud-read.cy.ts
@@ -1,4 +1,9 @@
 describe('Display customers', () => {
+    
+    before('Empty table', () => {
+        cy.visit('http://localhost/simple-crud-webapp/app/')
+        cy.deleteAllEntries()
+    })
 
     it('[tc-crud-005] shows customer list with existing records', () => {
         cy.visit('http://localhost/simple-crud-webapp/app/')
@@ -9,21 +14,9 @@ describe('Display customers', () => {
         cy.get('[name="location"]').type(location)
         cy.get('[name="save"]').click()
         // Expect customer to be displayed in table.
-        cy.get('table tbody tr')
-        .last()
-        .find('td')
-        .eq(0) // First column
-        .should('contain', name)
-
-        cy.get('table tbody tr')
-        .last()
-        .find('td')
-        .eq(1) // Second column
-        .should('contain', location)
+        cy.findLatestEntryAndCheckContent(name, location)
         // Clean up and delete the created customer.
-        cy.get('table tbody tr')
-        .last()
-        .find('td').contains('Delete').click()
+        cy.findLatestEntryAndDeleteIt()
     })
 
     it('[tc-crud-007] shows empty customer list', () => {
